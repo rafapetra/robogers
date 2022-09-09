ee10e37f0d515e3a30b9c59f47a94821e0fd4260
Rafael Petrachini <raffp@me.com>
2022-09-09 15:44:00 -0700

    Add tests for nummbers 1, 2, 3, the finalized function and reset button on index.html

--- js/script.js
+++ js/script.js
@@ -1,18 +1,51 @@
-window.onload = function() {
+// B LOGIC
 
-let form = document.querySelector('form');
 
-// get the value from the input
-form.onsubmit = function beep (input) {
-  input.preventDefault();
-  const userInput = document.getElementById("input").value;
-  const userInputNumber = parseInt(userInput);
-  document.body.append(userInputNumber);
+// UI Logic
+window.onload = function() { 
 
-const inputArray = [];
-for (let i = 0; i <= userInputNumber; i++) {
-  inputArray.push(i);
-  console.log(inputArray);
+  let form = document.querySelector('form');
+  let resetBtn = document.querySelector('button#reset');
+  let results = document.querySelector('div#results');
+
+  form.onsubmit = function beeper(getInput) {
+    getInput.preventDefault();
+  // showing reset button and result div
+    results.removeAttribute("class");
+    resetBtn.removeAttribute("class");
+// getting input
+    const userInput = document.getElementById("input").value;
+// array for input numbers
+    let inputArray = [];
+    for (let i = 0; i <= userInput; i++) {
+      inputArray.push(i);
+      }
+  
+// function for replacing numbers
+  let beep = (function(number) {
+    if (inputArray[number].toString().includes(1)) {
+      inputArray[number] = "Beep";
+    } else if (inputArray[number].toString().includes(2)) {
+      inputArray[number] = "Boop!";
+    } else if (inputArray[number].toString().includes(3)) {
+      inputArray[number] = "Won't you be my neighbor?";
     }
+    })
+
+// calling it on array of numbers
+    inputArray.forEach(beep);
+    document.querySelector('span#print').innerText = (inputArray);
   }
+
+// reset button function, cleans input values and hides resul div
+resetBtn.addEventListener("click", function() {
+    results.setAttribute("class", "hidden");
+    document.getElementById("input").value = null;
+
+  });
 }
+
+
+  
+
+
