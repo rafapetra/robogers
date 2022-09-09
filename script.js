0e6334d445594277d0711f8394d71fa32070dd70
Rafael Petrachini <raffp@me.com>
2022-09-08 20:09:49 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function beep (input) {
+
+  const inputArray = [];
+  for (let i = 0; i <= input; i++) {
+    inputArray.push(i);
+    console.log(inputArray);
+      } 
+      
+if (inputArray.includes(1)) {
+  inputArray.push(8);
+  document.body.append(inputArray);
+}
+
+
+}
+    
