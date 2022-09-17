7e392b4784dad9f1b65d4f76bc2281a234fbe6f1
Rafael Petrachini <raffp@me.com>
2022-09-17 12:50:14 -0700

    Fix JS logic, logic order and HTML indentation

--- js/script.js
+++ js/script.js
@@ -1,39 +1,37 @@
 window.onload = function () {
+  //BUSINESS LOGIC
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
   let results = document.querySelector("div#results");
+  let inputArray = [];
 
-  // submitting input
+  // function for replacing numbers
+  let beep = function (number) {
+    if (inputArray[number].toString().includes(3)) {
+      inputArray[number] = " Won't you be my neighbor?";
+    } else if (inputArray[number].toString().includes(2)) {
+      inputArray[number] = " Boop!";
+    } else if (inputArray[number].toString().includes(1)) {
+      inputArray[number] = " Beep!";
+    }
+  };
+
+  // UI LOGIC
+
   form.onsubmit = function beeper(getInput) {
     getInput.preventDefault();
     // showing reset button and result div
-    results.removeAttribute("class");
-    resetBtn.removeAttribute("class");
-    // getting input
-    const userInput = document.getElementById("input").value;
-    // array for input numbers
-    let inputArray = [];
+    let userInput = document.querySelector("input#input").value;
     for (let i = 0; i <= userInput; i++) {
       inputArray.push(i);
     }
-
-    // function for replacing numbers
-    let beep = function (number) {
-      if (inputArray[number].toString().includes(1)) {
-        inputArray[number] = " Beep";
-      } else if (inputArray[number].toString().includes(2)) {
-        inputArray[number] = " Boop!";
-      } else if (inputArray[number].toString().includes(3)) {
-        inputArray[number] = " Won't you be my neighbor?";
-      }
-    };
-
-    // calling it on array of numbers
     inputArray.forEach(beep);
     document.querySelector("div#result").innerText = inputArray;
+    // reset button function, cleans input values and hides resul div
+    results.removeAttribute("class");
+    resetBtn.removeAttribute("class");
   };
 
-  // reset button function, cleans input values and hides resul div
   resetBtn.addEventListener("click", function () {
     results.setAttribute("class", "hidden");
     document.getElementById("input").value = null;
